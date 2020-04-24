3137f19b00a6ceb164842a1245cbaa5c57d6c69f
William Major <epiphone1995@gmail.com>
2020-04-23 22:01:47 -0700

    removed more SQLite references

--- CommandLine/GRM/Program.cs
+++ CommandLine/GRM/Program.cs
@@ -1,8 +1,6 @@
 ﻿using RFID.Notifications;
 using System;
 using System.Collections.Generic;
-using System.Data.SQLite;
-using System.Linq;
 using System.Collections.Concurrent;
 using System.Threading;
 using System.IO;
