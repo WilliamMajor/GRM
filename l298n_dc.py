c2783480af154b87843350557f368a966093ca41
Austin <austintran333@yahoo.com>
2020-04-28 18:46:18 -0700

    ree

--- l298n_dc.py
+++ l298n_dc.py
@@ -4,7 +4,7 @@ from time import sleep
 en1 = 25 #right back enable
 in1 = 26 #right back wheel (+) input
 in2 = 23 #right back wheel (-) input
-en2 = 27 #right front enable
+en2 = 18 #right front enable
 in3 = 16 #right front wheel (+) input
 in4 = 17 #right front wheel (-) input
 temp1=1
@@ -15,18 +15,28 @@ GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
 GPIO.setup(in3,GPIO.OUT)
 GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(en1,GPIO.OUT)
+GPIO.setup(en1,GPIO.OUT) #ena
+GPIO.setup(en2,GPIO.OUT) #enb
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
 p=GPIO.PWM(en1,1000)
+p2 = GPIO.PWM(en2, 1000)
 
-p.start(10)
+p.start(30)
+p2.start(30)
 print("\n")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
 
 while(1):
-
+    print(GPIO.input(en1))
+    print(GPIO.input(in1))
+    print(GPIO.input(in2))
+    print(GPIO.input(in3))
+    print(GPIO.input(in4))
+    print(GPIO.input(en2))
     x=input()
     
     if x=='r':
@@ -34,11 +44,15 @@ while(1):
         if(temp1==1):
          GPIO.output(in1,GPIO.HIGH)
          GPIO.output(in2,GPIO.LOW)
+         GPIO.output(in3,GPIO.HIGH)
+         GPIO.output(in4,GPIO.LOW)
          print("forward")
          
         else:
          GPIO.output(in1,GPIO.LOW)
          GPIO.output(in2,GPIO.HIGH)
+         GPIO.output(in3,GPIO.LOW)
+         GPIO.output(in4,GPIO.HIGH)
          print("backward")
 
 
@@ -46,34 +60,44 @@ while(1):
         print("stop")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.LOW)
+        
 
     elif x=='f':
         print("forward")
         GPIO.output(in1,GPIO.HIGH)
         GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in4,GPIO.LOW)
         temp1=1
 
     elif x=='b':
         print("backward")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.HIGH)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.HIGH)
         temp1=0
 
     elif x=='l':
         print("low")
         p.ChangeDutyCycle(25)
-
+        p2.ChangeDutyCycle(25)
     elif x=='m':
         print("medium")
         p.ChangeDutyCycle(50)
+        p2.ChangeDutyCycle(50)
 
     elif x=='h':
         print("high")
         p.ChangeDutyCycle(75)
-     
+        p2.ChangeDutyCycle(75)
     
     elif x=='e':
         GPIO.cleanup()
+        p.stop()
+        p2.stop()
         print("GPIO Clean up")
         break
     
