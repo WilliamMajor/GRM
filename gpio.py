43d2d95ebbf997c628ee4aea65031ba4c15dbc17
Austin <austintran333@yahoo.com>
2020-05-02 20:09:57 -0700

    added sensor code for 3 sensors

--- gpio.py
+++ gpio.py
@@ -1,12 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-trigger = 18 #trigger pin number
-echo = 24 #echo pin number
+trigger = 5 #trigger pin number
+echo = 6 #echo pin number
+echo2 = 19
+echo3 = 9
+sample_time = .3
 GPIO.setup(trigger, GPIO.OUT)
 GPIO.setup(echo, GPIO.IN)
+GPIO.setup(echo2, GPIO.IN)
+GPIO.setup(echo3, GPIO.IN)
 def get_distance():
-    time.sleep(1)
+    time.sleep(sample_time)
     GPIO.output(trigger, True)
     time.sleep(0.00001)
     GPIO.output(trigger,False)
@@ -23,12 +28,53 @@ def get_distance():
     elapsed = stop - start
     distance = elapsed * 17150 #speed of sound is 34300 cm/s
     return distance
+
+def get_distance2():
+    time.sleep(sample_time)
+    GPIO.output(trigger, True)
+    time.sleep(0.00001)
+    GPIO.output(trigger,False)
+    start = time.time()
+    stop = time.time()
+    while GPIO.input(echo2) == 0:
+        start = time.time()
     
+    
+    while GPIO.input(echo2) == 1:
+        stop = time.time()
+        
+    
+    elapsed = stop - start
+    distance = elapsed * 17150 #speed of sound is 34300 cm/s
+    return distance
+
+def get_distance3():
+    time.sleep(sample_time)
+    GPIO.output(trigger, True)
+    time.sleep(0.00001)
+    GPIO.output(trigger,False)
+    start = time.time()
+    stop = time.time()
+    while GPIO.input(echo3) == 0:
+        start = time.time()
+    
+    
+    while GPIO.input(echo3) == 1:
+        stop = time.time()
+        
+    
+    elapsed = stop - start
+    distance = elapsed * 17150 #speed of sound is 34300 cm/s
+    return distance
 if __name__ == '__main__':
     try:
         while(True):
             dist = get_distance()
+            dist2 = get_distance2()
+            dist3 = get_distance3()
             print(f"Distance: {round(dist,2)} cm")
+            print(f"Distance2: {round(dist2,2)} cm")
+            print(f"Distance3: {round(dist3,2)} cm")
             
     except KeyboardInterrupt:
         print("Measurement stopped by user")
