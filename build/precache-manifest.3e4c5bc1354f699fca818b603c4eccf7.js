fcd49fedafba23cdc2373dbe494b751ca400cef5
Rohan1304 <prohan14498@gmail.com>
2020-05-06 16:41:49 -0700

    Spartan React App
    Back-end logic implementation to take in and parse csv file in reactJs to display data on local host server.

---
+++ build/precache-manifest.3e4c5bc1354f699fca818b603c4eccf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd76134c026db05b91ba87fa9fcd580",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95f6f8d70becdec6c991",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "499ecdf3043c9c4c86d8",
+    "url": "/static/js/2.fb1aea2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb1aea2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f6f8d70becdec6c991",
+    "url": "/static/js/main.61075b8b.chunk.js"
+  },
+  {
+    "revision": "4939a367dfc3759fbcd7",
+    "url": "/static/js/runtime-main.686e6d1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
